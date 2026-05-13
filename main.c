e5b269d7c9059713ca3911b7642fca29377c2844
Christopher-Espin <christopher.espin@udla.edu.ec>
2026-05-13 14:24:42 -0500

    Segundo Versionamiento

--- main.c
+++ main.c
@@ -2,9 +2,13 @@
 
 int main (int argc, char *argv[]) {
     int numeros[] = {2, 4, 6, 8, 10};
+    int *ptr;
+    ptr = &numeros[0];
+    //printf("%d\n", *ptr);
 
     for (int i = 0; i < 5; i++) {
         printf("%d ", numeros[i]);
+        printf("%d\n", *(ptr + i));
     }
     return 0;
 }
