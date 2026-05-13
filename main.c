b632c19da93475d7f4decb79b25fd45a5c85a79f
Christopher-Espin <christopher.espin@udla.edu.ec>
2026-05-13 15:26:58 -0500

    Ultima version 1er ejercicio

--- main.c
+++ main.c
@@ -4,11 +4,11 @@ int main (int argc, char *argv[]) {
     int numeros[] = {2, 4, 6, 8, 10};
     int *ptr;
     ptr = &numeros[0];
-    //printf("%d\n", *ptr);
+
+    *(ptr + 4) = 0;
 
     for (int i = 0; i < 5; i++) {
         printf("vector[%d] = %d %d\n", i + 1, numeros[i], *(ptr + i));
-        printf("%d\n", *(ptr + i));
     }
     return 0;
 }
